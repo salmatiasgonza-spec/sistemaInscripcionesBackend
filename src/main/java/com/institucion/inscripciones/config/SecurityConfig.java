79100d9570fa122352879939684f49c55e4531de
salmatiasgonza-spec <salmatias.gonza@gmail.com>
2025-11-04 11:16:47 -0300

    Update SecurityConfig.java

--- src/main/java/com/institucion/inscripciones/config/SecurityConfig.java
+++ src/main/java/com/institucion/inscripciones/config/SecurityConfig.java
@@ -39,7 +39,8 @@ public class SecurityConfig {
           "/swagger-ui/**",
           "/swagger-ui.html",
           "/actuator/**",
-          "/error"
+          "/error",
+          "/api/reportes/**" // lo agregue para solucionar los reportes
         ).permitAll()
         .anyRequest().authenticated()
       )
